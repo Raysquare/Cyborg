1fdfccc183e1470f284a561a217a4d4739d4add1
Terry <terryyuan01@gmail.com>
2019-09-29 14:52:13 +1000

    Movement
    added player controller and started on scripts for movement

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+    }
+}
