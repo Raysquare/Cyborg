4c32e3594e42f1ebbfe6af5519eadc065940f112
Terry <terryyuan01@gmail.com>
2019-10-06 15:46:48 +1100

    Simple projectile

--- Assets/CyborgBehaviour.cs
+++ Assets/CyborgBehaviour.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CyborgBehaviour : MonoBehaviour
 {
 
+    public GameObject projectilePrefab;
+
     public float speed;
     public float rotationSpeed;
+    private float rotation;
     public float gravity;
     public GameObject cyborg;
     Vector3 moveDirection = Vector3.zero;
@@ -19,19 +22,88 @@ public class CyborgBehaviour : MonoBehaviour
     {
         _controller = cyborg.GetComponent<CharacterController>();
         _animator = cyborg.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Movement();
+        GetInput();
+
         
+    }
+
+    public void Movement()
+    {
+
+        //if (_controller.isGrounded)
+        //{
         if (Input.GetKey(KeyCode.W))
         {
-            moveDirection = new Vector3(0,0,1);
-            moveDirection = moveDirection * speed;
+            changeDirection(1);
         }
-        
+        else if (Input.GetKey(KeyCode.S))
+        {
+            changeDirection(-1);
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            _animator.SetBool("Running", false);
+            moveDirection = new Vector3(0, 0, 0);
+            _animator.SetInteger("Condition", 0);
+        }
+        //}
+
+        this.rotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, this.rotation, 0);
+
         moveDirection.y -= gravity * Time.deltaTime;
         _controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    public void changeDirection(int x)
+    {
+        _animator.SetBool("Running", true);
+        moveDirection = new Vector3(0, 0, x);
+        moveDirection = moveDirection * speed;
+        moveDirection = transform.TransformDirection(moveDirection);
+        _animator.SetInteger("Condition", 1);
+    }
+
+    public void GetInput()
+    {
+        if (_controller.isGrounded)
+        {
+            if (Input.GetMouseButton(0))
+            {   
+                if (_animator.GetBool("Running") == true)
+                {
+                    _animator.SetBool("Running", false);
+                    _animator.SetInteger("Condition", 0);
+                }
+                else if (_animator.GetBool("Running") == false)
+                {
+                    Attack();
+                }
+                
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        _animator.SetBool("Attacking", true);
+        _animator.SetInteger("Condition", 3);
+        GameObject projectile = Instantiate<GameObject>(projectilePrefab);
+        projectile.transform.position = this.gameObject.transform.position;
+    }
+
+    public IEnumerator AttackRoutine()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
